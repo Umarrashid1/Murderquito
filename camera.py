29cfba8b081d5edcea8e113ed1386c4b75787f89
umarrashid1 <umarrashid@hotmail.dk>
2023-11-06 16:51:07 +0100

    added camera test

--- camera.py
+++ camera.py
@@ -1,1 +1,30 @@
+import cv2
 
+# Create a VideoCapture object to access the camera
+cap = cv2.VideoCapture(0)  # 0 indicates the default camera (Raspberry Pi camera)
+
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+while True:
+    # Read a frame from the camera
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Error: Could not read frame.")
+        break
+
+    # Convert the frame to grayscale
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the grayscale frame
+    cv2.imshow('Grayscale Camera Feed', gray_frame)
+
+    # Exit the loop if the 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the camera and close the OpenCV window
+cap.release()
+cv2.destroyAllWindows()
