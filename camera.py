c45fe6a58b96e32acac34dea0817cde212c7ab40
umarrashid1 <umarrashid@hotmail.dk>
2023-11-09 12:32:38 +0100

    added servo.py

--- camera.py
+++ camera.py
@@ -1,10 +1,9 @@
 import cv2
 from picamera2 import Picamera2
-picam2 = Picamera2()
-picam2.configure("preview")
-picam2.start()
+camera = Picamera2()
+camera.start()
 while True:
-    frame  = picam2.capture_array()
+    frame  = camera.capture_array()
     cv2.imshow("camera", frame)
     if cv2.waitKey(1)==ord('q'):
         break
