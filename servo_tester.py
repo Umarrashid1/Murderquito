a63272e517921814e47db6195554ba756ac5cc6c
umarrashid1 <umarrashid@hotmail.dk>
2023-11-14 13:27:50 +0100

    changes

--- servo_tester.py
+++ servo_tester.py
@@ -2,9 +2,7 @@ import time
 import io
 from servo import Servo
 
-
-def main():
-    servo_x = Servo("x")
-    time.sleep(5)
-    servo_x.move(180)
-    servo_x.stop()
+servo_x = Servo("x")
+time.sleep(5)
+servo_x.move(180)
+servo_x.stop()
