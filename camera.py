6af7a3250c0a81ee2ba3275009fbb6d508dd35c0
umarrashid1 <umarrashid@hotmail.dk>
2023-11-15 11:53:10 +0100

    oop camera.py

--- camera.py
+++ camera.py
@@ -1,11 +1,24 @@
 import cv2
 from picamera2 import Picamera2
 
-camera = Picamera2()
-camera.start()
-while True:
-    frame  = camera.capture_array()
-    cv2.imshow("camera", frame)
-    if cv2.waitKey(1)==ord('q'):
-        break
-cv2.destroyAllWindows()
+
+class Camera:
+
+    def __init__(self):
+        self.camera = Picamera2()
+
+    def start(self):
+        self.camera.start
+
+    def autofocus(self):
+        autofocus
+
+    def run(self):
+        frame = self.camera.capture_array()
+        return frame
+
+    def show(self):
+        cv2.imshow("camera", self.frame)
+       # if cv2.waitKey(1)==ord('q'):
+        #    break
+    #cv2.destroyAllWindows()
