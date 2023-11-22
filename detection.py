d6433a323cf5cf9581452b93fe1dcc00c493c304
umarrashid1 <umarrashid@hotmail.dk>
2023-11-22 11:54:16 +0100

    bugfix

--- detection.py
+++ detection.py
@@ -14,7 +14,7 @@ class Detection:
     def __init__(self, cam):
         self.tracker = cv2.legacy.TrackerKCF.create()
         self.bbox = self.find_circle(cam.gray_frame())
-        self.init_tracker(self, cam)
+        self.init_tracker(cam)
     
     def init_tracker(self, cam):
         self.tracker.init(getattr(cam, 'frame'), self.bbox)
