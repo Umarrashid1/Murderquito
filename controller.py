dbf298d243644acae57e0804243d0995a17efde0
”draellemeistro” <and@hafsjold.com>
2023-12-06 00:51:28 +0100

    .

--- controller.py
+++ controller.py
@@ -15,22 +15,52 @@ else:
 cam = Camera(input_param)
 det = Detection(cam)
 servo_c = Servo_controller()
-##########
-# Kig i 
-##########
+
 while True:
     frame = cam.run()
     det.update_tracker(cam) #før vi overhovedet kan gå igang med det her, er der nogle ting vi skal vide ting vi skal vide: 
-    #   afstand til rotationspunktet om x-aksen
-    #   afstand til rotationspunktet om y-aksen
+    #   afstand fra kameramidten til rotationspunktet om x-aksen
+    #   afstand fra kameramidten til rotationspunktet om y-aksen
+        #se skitse i fuckinglorteclean
 
-    #hvis vi så får centreret laserpunktet i billedet. nemmest bare at gøre det brute-force style:
-    #
+    #vil også sige at vi skal bruge kamerakalibrering først
+    #så skal vi have centreret laserpunktet i billedet. nemmest bare at gøre det brute-force style:
+    #se servoCalibrator.center_laser_in_img(det, servo_x, servo_y) NOTE: servo x og y og servocontrolller..fgh
+
+    # vi kan udregne afstanden på et par måder. 
+
+        #hvis vi fysisk har målt afstanden til væg: 
+        # #servoCalibrator.calc_laser_angle_from_distance(det, servo_x, servo_y)
+        # så vi kan tjekke om tingene stemmer overens
+
+        #ellers kan vi bruge lidt snyd til at udregne en vinkel for midten. Hvis ikke servoerne allerede giver vinklerne:
+        # servoCalibrator.calc_laser_angle_when_centered(det, servo_x, servo_y)
+        # Når den er centreret BØR det være en retvinklet trekant. b = a × tan(β), derfor:
+        # servoCalibrator.calc_dist_angle(det, servo_x, servo_y)
+
+        #hvis laseren ikke er centreret kan man bruge noget pis med focal length osv. det er lort. Man kunne også benytte sig af at kameraet har en fov =80
+
+    # hvis vi kan finde ud af hvor mange pixels der går på ?? cm, så kan vi finde ud af afstande på væggen (kræver nok at kameraet er kalibreret. muligivs også omtanke for focal og lense Bullshit)
+    # med afstande kan vi hurtigere udregne vinkler til servoerne. tænk: hvis vi har vinkel for midten til myg (det har vi, hvis vi bruger de andre ting) samt vinkel for servo til midten, 
+        # så kan vi nemt finde vinklen fra laser til myg
+     #måske også notere hvor meget servoen bevæger sig med 1 grads drejning. giver maske hurtige respons senere.
+     # den kan også bare kontinuerligt følge den. Der skal vi også kende laserens placering, selv hvis den er slukket
+
+     #jeg er træt, men så burde vi nemt kunne kortlægge coordinater med laser vinkler
+
+     #så er der også det med at kameraet måske står lidt skævt, laseren måske sidder lidt skævt og fuck mig 
+
+    #det var det jeg ville tjekke over weekenden, men... fucking clean
+
     det.draw_boundingbox(cam)
 
     coordinates = det.get_center_coordinates()
     print(coordinates[0], coordinates[1])
-    servo_c.move(coordinates) #nej, det er dumt. kig nu på det jeg har lavet!
-    #der skal bruges trigonometri
+    servo_c.move(coordinates) 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+# ##########    se evt:     ######################
+#   https://www.youtube.com/watch?v=1CVmjTcSpIw
+#   https://www.youtube.com/watch?v=sW4CVI51jDY&t=1202s
+#   OneNote -> Vidensindsamling -> Computer Visualization og openCV -> GODE VIDEOER
+######################################
--- servo_calbration.py
+++ servo_calbration.py
@@ -5,7 +5,7 @@ import glob
 from camera import Camera
 import numpy as np
 from servo import Servo
-from detection import Detection
+from detection import Detector
 
 class ServoCalibrator:
     
@@ -15,42 +15,42 @@ class ServoCalibrator:
     # cam_focal_ratio = 1.75
     cam_to_wall = 30
 
-    def __init__(self, cam, cam_laser_axis_x_dist, cam_laser_axis_y_dist ) -> None:
-        self.cam_to_laser_axis_x = cam_laser_axis_x_dist
-        self.cam_to_laser_axis_y = cam_laser_axis_y_dist
-        self.cam_to_laser_origin_dist = math.sqrt(self.cam_to_laser_axis_x^2 + self.cam_to_laser_axis_y^2)
-        self.cam_px_width = cam.px_width
-        self.cam_px_height = cam.px_height
-        self.frame_center_coords = (cam.px_width/2, cam.px_height/2)
+    def __init__(self, detector = Detector, axis_x_dist = int, axis_y_dist = int) -> None:
+        self.dist_cam_axis_x = axis_x_dist
+        self.dist_cam_axis_y = axis_y_dist
+        self.dist_cam_axis_origin = math.sqrt(self.dist_cam_axis_x^2 + self.dist_cam_axis_y^2)
+        self.px_width = detector.frame.px_width
+        self.px_height = detector.frame.px_height
+        self.center_coords = (self.px_width/2, self.px_height/2)
 
 
 
-    def find_distance_to_wall(self, det = Detection, servo_x = None, servo_y = None):
+    def find_distance_to_wall(self, det = Detector, servo_x = None, servo_y = None):
         if det.find_black_rectangle() != False:
-            self.calc_dist_to_wall_using_box(det)
+            self.calc_dist_box(det)
         elif servo_x is Servo or servo_y is Servo:
-            self.calc_dist_to_wall_using_angle(det, servo_x = Servo, servo_y = Servo)
-        else: self.calc_dist_to_wall_using_checkerboard(det)
+            self.calc_dist_angle(det, servo_x = Servo, servo_y = Servo)
+        else: self.calc_dist_checkers(det)
         
-    def calc_dist_to_wall_using_checkerboard(NotImplemented): NotImplemented  #TODO    
-    def calc_dist_to_wall_using_box(): NotImplemented  #TODO: youtube metoden(?) elller kan dne her bruges #https://www.researchgate.net/publication/235196461_A_Simple_Method_for_Range_Finding_via_Laser_Triangulation      
-    def calc_dist_to_wall_using_angle(self, det = Detection, servo_x = None, servo_y = None):   #return bools værdi
+    def calc_dist_checkers(NotImplemented): NotImplemented  #TODO    
+    def calc_dist_box(): NotImplemented  #TODO: youtube metoden(?) elller kan dne her bruges #https://www.researchgate.net/publication/235196461_A_Simple_Method_for_Range_Finding_via_Laser_Triangulation      
+    def calc_dist_angle(self, det = Detector, servo_x = None, servo_y = None):   #return bools værdi
         #TODO: 
         #laseren skal enten vaere centreret, ellers skal den være i billedet om man skal omregne dimensioner til pixel
         # b = a × tan(β)
         cam_to_laser = 4
         if servo_x is Servo and servo_y is Servo:
-            # de to variable kanter i retvinklet skal <90 også, servo går 0..180, så find siden som kameraet er på
+            # de to variable kanter i retvinklet skal <90 også, servo går 0..180, så tag højde siden som kameraet er på
             angle_x = servo_x.angle # +/- self.angle_offsett_x ????? TODO
             angle_y = servo_y.angle # +/- self.angle_offsett_y ????? TODO
             if angle_x > 90: angle_x - 90 
             if angle_y > 90: angle_y - 90
             
-        laser_coord_x, laser_coord_y = det.find_laser_dot() #!!!!!!!!!!!!!
-        if ((laser_coord_x, laser_coord_y) == self.frame_center_coords):
+        dot_coord_x, dot_coord_y = det.find_laser_dot() #!!!!!!!!!!!!!
+        if ((dot_coord_x, dot_coord_y) == self.center_coords):
             # b = a × tan(β)
-            cam_to_wall_x = self.cam_to_laser_axis_y * math.tan(angle_y)
-            cam_to_wall_y = self.cam_to_laser_axis_x * math.tan(angle_x)
+            cam_to_wall_x = self.dist_cam_axis_y * math.tan(angle_y)
+            cam_to_wall_y = self.dist_cam_axis_x * math.tan(angle_x)
             #tjek om de giver den samme distance til væggen. returnerer om de giver den samme distance, og så en distance
             if cam_to_wall_x == cam_to_wall_y: 
                 self.cam_to_wall = cam_to_wall_x
@@ -59,107 +59,115 @@ class ServoCalibrator:
                 self.cam_to_wall = (cam_to_wall_x + cam_to_wall_y / 2)
                 return False, (cam_to_wall_x + cam_to_wall_y / 2)
             
-        elif laser_coord_y == self.frame_center_coords[1]: 
-            self.cam_to_wall = self.cam_to_laser_axis_x * math.tan(angle_x)
+        elif dot_coord_y == self.center_coords[1]: 
+            self.cam_to_wall = self.dist_cam_axis_x * math.tan(angle_x)
             return False, self.cam_to_wall
-        elif laser_coord_x == self.frame_center_coords[0]: 
-            self.cam_to_wall = self.cam_to_laser_axis_y * math.tan(angle_y)
+        elif dot_coord_x == self.center_coords[0]: 
+            self.cam_to_wall = self.dist_cam_axis_y * math.tan(angle_y)
             return False, self.cam_to_wall
         
         else: # do some conversion bullshit or center laser in frame idk
-            mid_to_dot_x = abs(laser_coord_x - self.frame_center_coords[0])
-            mid_to_dot_y = abs(laser_coord_y - self.frame_center_coords[1])
-            NotImplemented
-        
-        return False, self.cam_to_wall
+           # mid_to_dot_x = abs(dot_coord_x - self.center_coords[0])
+           # mid_to_dot_y = abs(dot_coord_y - self.center_coords[1])
+            #NotImplemented
+            return False, 00000
 
 
 
-    def find_dimension_conversion_factor(self, method_choice = str, detector = Detection, servo_x= None, servo_y= None,):
+    def find_dimension_conversion_factor(self, method_choice = str, detector = Detector, servo_x= None, servo_y= None,):
         if method_choice == "box":
-            self._px_cm_conversion_factor_known_square(detector)
+            self._calc_dim_conv_fact_known_square(detector)
         elif method_choice == "checkerboard":
-            self._px_cm_conversion_factor_checkerboard(detector)
+            self._calc_dim_conv_fact_checkerboard(detector)
         elif (servo_x is Servo or servo_y is Servo):
-            self._px_cm_conversion_factor_perpendicular_laser(detector, servo_x, servo_y,)
+            self._calc_dim_conv_fact_perpendicular_laser(detector, servo_x, servo_y,)
         else: return False
         
-    def _px_cm_conversion_factor_perpendicular_laser(self, detector = Detection, servo_x= None, servo_y= None):
+    def _calc_dim_conv_fact_perpendicular_laser(self, detector = Detector, servo_x= None, servo_y= None):
         px_to_cm_heigth_scale = False 
         px_to_cm_width_scale = False
 
+        #hvis laseren er lige på, så er afstand fra midten til laserdot, det samme som kamera til laser
+
         if servo_y is Servo and servo_y.angle == 90:
-            dot_offset_x = abs((self.cam_px_width/2)- detector.find_laser_dot('axis_y')) #abs for kun at få positv værdi
-            px_to_cm_width_scale = (dot_offset_x / self.cam_to_laser_axis_y) 
+            dot_offset_x = abs((self.px_width/2)- detector.find_laser_dot('axis_y')) #abs for kun at få positv værdi
+            px_to_cm_width_scale = (dot_offset_x / self.dist_cam_axis_y) 
         
         elif servo_x is Servo:
             return NotImplemented #TODO: make method  servo perpendicular
         
         if servo_x is Servo and servo_x.angle == 90:
-            dot_offset_y = abs((self.cam_px_height/2)- detector.find_laser_dot('axis_x')) #abs for kun at få positv værdi
-            px_to_cm_heigth_scale = (dot_offset_y / self.cam_to_laser_axis_x)
+            dot_offset_y = abs((self.px_height/2)- detector.find_laser_dot('axis_x')) #abs for kun at få positv værdi
+            px_to_cm_heigth_scale = (dot_offset_y / self.dist_cam_axis_x)
         
         elif servo_y is Servo:
             return NotImplemented #TODO: make method for servo perpendicular
         
         return px_to_cm_heigth_scale, px_to_cm_width_scale
         
-    def _px_cm_conversion_factor_checkerboard(self, detector = Detection): NotImplemented       
-    def _px_cm_conversion_factor_known_square(self, detector = Detection):NotImplemented
+    def _calc_dim_conv_fact_checkerboard(self, detector = Detector): NotImplemented       
+    def _calc_dim_conv_fact_known_square(self, detector = Detector):NotImplemented
 
 
-    def calc_laser_angle_from_distance(self, det = Detection, servo_x = None, servo_y = None):  #TODO:
+    def calc_laser_angle_from_distance(self, det = Detector, servo_x = None, servo_y = None):
     # Given α(angle of corner at wall): β(angle of laser corner) = 90 - α
     # β = arctan(b / a)     OR     β = arccot(a / b)
-
         #vinklen for hvor laser ville møde midten af kameraet
         #   β=arctan(b/a)
-        if(det.find_laser_dot == self.frame_center_coords):
-            self.laser_to_center_angle_origin = 90 - math.atan(self.cam_to_wall/self.cam_to_laser_origin_dist)
-            self.laser_to_center_angle_x = 90 - math.atan(self.cam_to_wall/self.cam_to_laser_axis_x)
-            self.laser_to_center_angle_y = 90 - math.atan(self.cam_to_wall/self.cam_to_laser_axis_y)
-        elif servo_x is Servo and servo_y is Servo: # Har det nogen værdi at prøve at finde afstanden hvis laseren ikke er centreret? de burde faktisk være det samme som dem over. måske værd at teste
-            if(servo_x.angle == 90): NotImplemented
-            if(servo_y.angle == 90): NotImplemented
-
-        #something with difference between angle from calculations and angle saved to servo
+        if(det.find_laser_dot == self.center_coords):
+            self.laser_to_center_angle_origin = 90 - math.atan(self.cam_to_wall/self.dist_cam_axis_origin)
+            self.laser_to_center_angle_x = 90 - math.atan(self.cam_to_wall/self.dist_cam_axis_x)
+            self.laser_to_center_angle_y = 90 - math.atan(self.cam_to_wall/self.dist_cam_axis_y)
+        else: return False
+        #Man kan også gøre noget lignende, hvis laseren er vinkelret med væggen, men det gider jeg ikke lige nu
         # maaske return et offset af en art?
-        if servo_x is Servo and servo_y is Servo:
+        if servo_x is Servo and servo_y is Servo:   #NOTE: Det skal nok bruges, men det gider jeg heller ikke
             if servo_y.angle != self.laser_to_center_angle_y:
                 self.angle_offsett_x = servo_x.angle - self.laser_to_center_angle_x
             if servo_x.angle != self.laser_to_center_angle_x:
                 self.angle_offsett_y = servo_y.angle - self.laser_to_center_angle_y
+            
+    def calc_laser_angle_when_centered(self, det = Detector, servo_x = None, servo_y = None):
+        #hvis laser vinkelret er = 90, så find vinkelforskel mellem det og når den peger på midten:
+        if(det.get_center_coordinates(en_fucking_laser_tak) == self.center_coords):
+            angle_for_center_y = 90 - servo_x.angle #tjek om det er den ene eller anden laser. alt efter drejing om akse, eller bevægelse langs
+            angle_for_center_x = 90 - servo_y.angle #same
+            # angle_for_origin = den kan man også finde 
+        return (angle_for_center_y, angle_for_center_x)
 
 
-    def center_laser_in_img(self, det = Detection, servo_x=Servo, servo_y=Servo):
+    def center_laser_in_img(self, det = Detector, servo_x=Servo, servo_y=Servo):
                #TODO: find elegant way of doing it. with and/or without trial&error / other way
         #ryk laser op/ned indtil den rammer midterlinjen
-        servo_x.move(90)
-        servo_y.move(90)
         x_coord = det.find_laser_dot('x')
         y_coord = det.find_laser_dot('y')
-
-        #NOTE: also, bare lige for at være sikker. hvor starter billedets koordina
-        #lasers cordinat hvis laser 90 90. x= center_of_frame +/- cm_conv_px(cam_to_laser)
-        if  x_coord == 1111:
-            self.calc_angle_for_frame_center(servo_y, x_coord)
-
-        """
-            while(det.find_laser_dot('x') > (self.cam_px_width)/2):
+        if (x_coord, y_coord) == self.center_coords:
+            return True
+        else:
+            servo_x.move(90)
+            servo_y.move(90)
+            #NOTE: also, bare lige for at være sikker. hvor starter billedets koordina
+            #lasers cordinat hvis laser 90 90. x= center_of_frame +/- cm_conv_px(cam_to_laser)
+                            #if  x_coord == 1111:
+                                #self.calc_angle_for_frame_center(servo_y, x_coord)
+            while(det.find_laser_dot('x') > (self.px_width)/2):
                 servo_x.move(servo_x.angle - 1)
-            while(det.find_laser_dot('x') < (self.cam_px_width)/2):
+            while(det.find_laser_dot('x') < (self.px_width)/2):
                 servo_x.move(servo_x.angle + 1)
-            while(det.find_laser_dot('y') > (self.cam_px_height)/2):
+            while(det.find_laser_dot('y') > (self.px_height)/2):
                 servo_y.move(servo_y.angle - 1)
-            while(det.find_laser_dot('y') < (self.cam_px_height)/2):
+            while(det.find_laser_dot('y') < (self.px_height)/2):
                 servo_x.move(servo_x.angle + 1)
-                """
-    def calc_angle_for_frame_center(self, det = Detection, servo = Servo ,coordinate = None): #TODO:det hænger ikke sammen
+
+
+            
+    
+    def calc_angle_for_frame_center(self, det = Detector, servo = Servo ,coordinate = None): #TODO:det hænger ikke sammen
         if servo.axis == "x":
-            dist_cam_to_axis = self.cam_to_laser_axis_y
-            if coordinate < self.cam_px_width/2 : gokbok = 1
+            dist_cam_to_axis = self.dist_cam_axis_y
+            if coordinate < self.px_width/2 : gokbok = 1
         elif servo.axis == "y":
-            dist_cam_to_axis = self.cam_to_laser_axis_x
+            dist_cam_to_axis = self.dist_cam_axis_x
             det.find_laser_dot('x')
 
         angle_to_center_laser = 1 #111111111
