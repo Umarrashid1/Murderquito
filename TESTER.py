25f5f36741ace783ec4da6c9ee0f66dd43b1bebf
umarrashid1 <umarrashid@hotmail.dk>
2023-12-06 15:56:37 +0100

    fixed TESTER

--- TESTER.py
+++ TESTER.py
@@ -1,4 +1,17 @@
+from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import AngularServo
+from time import sleep
+
+factory = PiGPIOFactory()
+joint_base = AngularServo(14, min_angle=-90, max_angle=90, pin_factory=factory)
+angle_base = 0
+while True:
+    angle = int(input("Angle: "))
+
+    angle_base = angle
+    joint_base.angle = angle_base
+
+    print(angle, angle_base)from gpiozero import AngularServo
 from time import sleep
 
 joint_base = AngularServo(14, min_angle=-90, max_angle=90)
