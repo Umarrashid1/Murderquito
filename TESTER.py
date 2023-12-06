4412a52eed5a1fd56664f45f7ecf1e951ec63f38
umarrashid1 <umarrashid@hotmail.dk>
2023-12-06 15:55:18 +0100

    added TESTER.py

---
+++ TESTER.py
@@ -0,0 +1,17 @@
+from gpiozero import AngularServo
+from time import sleep
+
+joint_base = AngularServo(14, min_angle=-90, max_angle=90)
+joint_top = AngularServo(15, min_angle=-90, max_angle=90)
+angle_base = 0
+angle_top = 0
+while True:
+    angle = int(input())
+  
+    angle_base = angle
+    joint_base.angle = angle_base
+
+    angle_top = 90-angle_base
+    joint_top.angle = angle_top
+
+    print(angle, angle_base, angle_top)
