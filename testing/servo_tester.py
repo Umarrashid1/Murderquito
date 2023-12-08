4deeba242f2ad0940b3080532592d9407adda1ab
JSanizi <Janiceakiza@yahoo.com>
2023-12-08 15:35:33 +0100

    Stillet servoerne ti deres 90. grad

--- testing/servo_tester.py
+++ testing/servo_tester.py
@@ -1,20 +1,20 @@
 import inspect
 import sys
 import os
+import pigpio
 
-#til at tilgå ting i parent dir
+# til at tilgaa ting i parent dir
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
 import time
-import io
 from servo import Servo
-
-servo_x = Servo("x")
-time.sleep(5)
-servo_x.move(179)
-time.sleep(5)
-servo_x.move(90)
-time.sleep(5)
-servo_x.stop()
+io = pigpio.pi()
+servo_x = Servo("x", io)
+servo_y = Servo("y", io)
+for i in range(10):
+    servo_x.move(i+90)  # x står i 90 grad
+    servo_y.move(i+119)  # y står i 90 grad
+    print(i)
+    time.sleep(0.5)
