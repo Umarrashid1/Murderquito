7906033c101b706dbb49c4af8189575fdc827f8c
”draellemeistro” <and@hafsjold.com>
2023-12-13 03:27:29 +0100

    laser class added

--- laser.py
+++ laser.py
@@ -7,19 +7,39 @@ class Laser:
 
     def __init__(self):
         #self.io = pigpio.pi()
-        self.pin = 17
+        self.pin = 16
         GPIO.setmode(GPIO.BCM) 
-        GPIO.setup(17, GPIO.OUT)
+        GPIO.setup(self.pin, GPIO.OUT)
         #self.io.set_mode(self.pin, pigpio.OUTPUT)
         self.laser_status = 0
+        self._set_laser_off()
 
 
-    def toggle_laser(self):
-        if self.laser_status == 0:
-            self.laser_status = 1
-            GPIO.output(17, True)
+    def toggle_laser(self, command = None):
+        if command is not None:
+            if command == 1:
+                self._set_laser_on()
+                return True
+            elif command == 0: 
+                self._set_laser_off()
+                return True
+            else:
+                print("toggle_laser() Error: Invalid argument.")
+        elif self.laser_status == 0:
+            self._set_laser_on()
             return True
         else:
-            self.laser_status = 0
-            GPIO.output(17, False)
-            return True
+            self._set_laser_off()
+            return False
+        
+    def _set_laser_on(self):
+        self.laser_status = 1
+        GPIO.output(self.pin, True)
+        return True
+    
+    def _set_laser_off(self):
+        self.laser_status = 0
+        GPIO.output(self.pin, False)
+        return True
+    
+    def fire_laser_pulse(self): NotImplemented
