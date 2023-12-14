7bd0342df4b0827f56f00b7288fc1284fba62a34
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2023-12-14 02:00:47 +0100

    center laser cn now run in loop outside controller

--- servo_calibrator.py
+++ servo_calibrator.py
@@ -18,8 +18,8 @@ class ServoCalibrator:
     cam_to_wall = 0
 
     def __init__(self, cam = Camera, servo_c = Servo_controller) -> None:
-        self.cam_to_tilt_dist = 4.3
-        self.cam_to_pan_dist = 4.6
+        self.cam_to_tilt_dist = 43
+        self.cam_to_pan_dist = 46
         self.dist_cam_axis_origin = math.sqrt(self.cam_to_tilt_dist*self.cam_to_tilt_dist + self.cam_to_pan_dist*self.cam_to_pan_dist)
         self.px_height, self.px_width, self.channels = cam.frame.shape
         self.center = (self.px_width/2, self.px_height/2)
@@ -37,7 +37,7 @@ class ServoCalibrator:
             time.sleep(2)
 
 
-    def center_laser(self, frame, det, servo_c, dist_to_wall = None):
+    def center_laser(self, cam, det, servo_c, dist_to_wall = None):
         center = self.center
 
         if dist_to_wall is not None:
@@ -45,58 +45,79 @@ class ServoCalibrator:
           angles = self.calc_centerangle_from_distance( det, servo_c, dist_to_wall)
           servo_c.pan.move(angles[0])
           servo_c.tilt.move(angles[1])
-          return True  
-        else:  
-            las_coords = det.find_red(frame)
-            if las_coords is False:
-                print("Error: no laser found!")
-            if(las_coords[0] != center[0]):
-                if(las_coords[0] > center[0] and self.pan_angle < 180):
-                    if(las_coords[0] > center[0] + 20 and self.pan_angle < 180):
-                        self.pan_angle = self.pan_angle + 1
-                        servo_c.pan_servo.move(self.pan_angle)
-                        time.sleep(1)
-                    else:
-                        self.pan_angle = self.pan_angle + 0.1
-                        servo_c.pan_servo.move(self.pan_angle)
-                        time.sleep(0.5)
-                elif(las_coords[0] < center[0] and self.pan_angle > 0):
-                    if(las_coords[0] < center[0] - 20 and self.pan_angle > 0):
-                        self.pan_angle = self.pan_angle - 1
-                        servo_c.pan_servo.move(self.pan_angle)
-                        time.sleep(1)
-                    else:
-                        self.pan_angle = self.pan_angle - 0.1
-                        servo_c.pan_servo.move(self.pan_angle)
-                        time.sleep(0.5)
-            if(las_coords[1] != center[1]):
-                if(las_coords[1] < center[1] and self.tilt_angle < 180):
-                    if(las_coords[1] < center[1]-20 and self.tilt_angle < 180):
-                        self.tilt_angle = self.tilt_angle + 1
-                        servo_c.tilt_servo.move(self.tilt_angle)
-                        time.sleep(1)
-                    else:
-                        self.tilt_angle = self.tilt_angle + 0.1
-                        servo_c.tilt_servo.move(self.tilt_angle)
-                        time.sleep(0.5)
-                elif(las_coords[1] > center[1] and self.tilt_angle > 0):
-                    if(las_coords[1] > center[1] + 20 and self.tilt_angle < 180):
-                        self.tilt_angle = self.tilt_angle - 1
-                        servo_c.tilt_servo.move(self.tilt_angle)
-                        time.sleep(1)
-                    else:
-                        self.tilt_angle = self.tilt_angle - 0.1
-                        servo_c.tilt_servo.move(self.tilt_angle)
-                        time.sleep(0.5)            
-            if(las_coords[1] < center[1] + 5 and las_coords[1] > center[1] - 5):
-                if(las_coords[0] < center[0] + 5 and las_coords[0] > center[0] - 5):
-                    return True
-            else: return False
+          return True
+          
+        else: 
+            las_is_centered = False 
+            while las_is_centered is False:
+                frame = cam.run()
+                las_coords = det.find_red(frame)
+                if las_coords is False:
+                    print("Error: no laser found!")
+                if(las_coords[0] != center[0]):
+                    if(las_coords[0] > center[0] and self.pan_angle < 180):
+                        if(las_coords[0] > center[0] + 20 and self.pan_angle < 180):
+                            self.pan_angle = self.pan_angle + 1
+                            servo_c.pan_servo.move(self.pan_angle)
+                            time.sleep(1)
+                        elif(las_coords[0] > center[0] + 10 and self.pan_angle < 180):
+                            self.pan_angle = self.pan_angle + 0.1
+                            servo_c.pan_servo.move(self.pan_angle)
+                            time.sleep(0.5)
+                        else:
+                            self.pan_angle = self.pan_angle + 0.5
+                            servo_c.pan_servo.move(self.pan_angle)
+                            time.sleep(0.2)
+                    elif(las_coords[0] < center[0] and self.pan_angle > 0):
+                        if(las_coords[0] < center[0] - 20 and self.pan_angle > 0):
+                            self.pan_angle = self.pan_angle - 1
+                            servo_c.pan_servo.move(self.pan_angle)
+                            time.sleep(1)
+                        elif(las_coords[0] < center[0] - 10 and self.pan_angle > 0):
+                            self.pan_angle = self.pan_angle - 0.1
+                            servo_c.pan_servo.move(self.pan_angle)
+                            time.sleep(0.5)
+                        else:
+                            self.pan_angle = self.pan_angle - 0.05
+                            servo_c.pan_servo.move(self.pan_angle)
+                            time.sleep(0.2)
+                if(las_coords[1] != center[1]):
+                    if(las_coords[1] < center[1] and self.tilt_angle < 180):
+                        if(las_coords[1] < center[1]-20 and self.tilt_angle < 180):
+                            self.tilt_angle = self.tilt_angle + 1
+                            servo_c.tilt_servo.move(self.tilt_angle)
+                            time.sleep(1)
+                        elif(las_coords[1] < center[1]-10 and self.tilt_angle < 180):
+                            self.tilt_angle = self.tilt_angle + 0.1
+                            servo_c.tilt_servo.move(self.tilt_angle)
+                            time.sleep(0.5)
+                        else:
+                            self.tilt_angle = self.tilt_angle + 0.05
+                            servo_c.tilt_servo.move(self.tilt_angle)
+                            time.sleep(0.2)
+                    elif(las_coords[1] > center[1] and self.tilt_angle > 0):
+                        if(las_coords[1] > center[1] + 20 and self.tilt_angle < 180):
+                            self.tilt_angle = self.tilt_angle - 1
+                            servo_c.tilt_servo.move(self.tilt_angle)
+                            time.sleep(1)
+                        elif(las_coords[1] > center[1] + 10 and self.tilt_angle < 180):
+                            self.tilt_angle = self.tilt_angle - 0.1
+                            servo_c.tilt_servo.move(self.tilt_angle)
+                            time.sleep(0.5)
+                        else:
+                            self.tilt_angle = self.tilt_angle - 0.05
+                            servo_c.tilt_servo.move(self.tilt_angle)
+                            time.sleep(0.2)            
+                if(las_coords[1] < center[1] * 1.05 and las_coords[1] > center[1] *0.95):
+                    if(las_coords[0] < center[0] *1.05 and las_coords[0] > center[0] *0.95):
+                        las_is_centered = True
+                        return True
+
             
             
 
-    def find_indiv_center_angles(self,frame, det, servo_c, chosen_servo = None):
-        center = self.center_coords
+    def find_indiv_center_angles(self,cam, det, servo_c, chosen_servo = None):
+        center = self.center
 
         if chosen_servo == 'x':
             servo = 0
@@ -104,43 +125,46 @@ class ServoCalibrator:
         elif chosen_servo == 'y':
             servo = 1
             angle = self.tilt_angle
-
-        las_cord = det.find_red(frame)
-        print(las_cord)
-        if(las_cord[servo] != center[servo]):
-            if(las_cord[servo] > center[servo]):
-                if(las_cord[servo] > center[servo] + 20):
-                    angle = angle - 1
-                    servo_c.servoes[servo].move(angle)
-                    time.sleep(1)
-                elif(las_cord[servo] > center[servo] + 10):
-                    angle = angle - 0.1
-                    servo_c.servoes[servo].move(angle)
-                    time.sleep(0.5)
-                else:
-                    angle = angle - 0.05
-                    servo_c.servoes[servo].move(angle)
-                    time.sleep(0.5)
-            elif(las_cord[servo] < center[servo]):
-                if(las_cord[servo] < center[servo] - 20):
-                    angle = angle + 1
-                    servo_c.servoes[servo].move(angle)
-                    time.sleep(1)
-                elif(las_cord[servo] < center[servo] - 10):
-                    angle = angle + 0.1
-                    servo_c.servoes[servo].move(angle)
-                    time.sleep(0.5)
-                else:
-                    angle = angle + 0.05
-                    servo_c.servoes[servo].move(angle)
-                    time.sleep(0.5)
-            if chosen_servo == 'y':
-                    self.tilt_angle = angle
-            elif chosen_servo == 'x':
-                    self.pan_angle = angle
-            if(las_cord[servo] < center[servo] + 2 and las_cord[servo] > center[servo] - 2):
-                return True
-            else: return False
+        
+        center_angle_found = False
+        while center_angle_found is False:
+            frame = cam.run()
+            las_cord = det.find_red(frame)
+            print(las_cord)
+            if(las_cord[servo] != center[servo]):
+                if(las_cord[servo] > center[servo]):
+                    if(las_cord[servo] > center[servo] + 20):
+                        angle = angle - 1
+                        servo_c.servoes[servo].move(angle)
+                        time.sleep(1)
+                    elif(las_cord[servo] > center[servo] + 10):
+                        angle = angle - 0.1
+                        servo_c.servoes[servo].move(angle)
+                        time.sleep(0.5)
+                    else:
+                        angle = angle - 0.05
+                        servo_c.servoes[servo].move(angle)
+                        time.sleep(0.5)
+                elif(las_cord[servo] < center[servo]):
+                    if(las_cord[servo] < center[servo] - 20):
+                        angle = angle + 1
+                        servo_c.servoes[servo].move(angle)
+                        time.sleep(1)
+                    elif(las_cord[servo] < center[servo] - 10):
+                        angle = angle + 0.1
+                        servo_c.servoes[servo].move(angle)
+                        time.sleep(0.5)
+                    else:
+                        angle = angle + 0.05
+                        servo_c.servoes[servo].move(angle)
+                        time.sleep(0.5)
+                if chosen_servo == 'y':
+                        self.tilt_angle = angle
+                elif chosen_servo == 'x':
+                        self.pan_angle = angle
+                if(las_cord[servo] < center[servo] + 2 and las_cord[servo] > center[servo] - 2):
+                    return True
+        
         
     def calc_centerangle_from_distance(self, det, servo_c, dist_to_wall):
         # Given α(angle of corner at wall): β(angle of laser corner) = 90 - α
@@ -163,10 +187,15 @@ class ServoCalibrator:
         print(tilt_angle)
         print('hhhhhhhhhmmmmmmmmm')    
         # b = a × tan(β)
-        cam_to_wall_x = math.tan(tilt_angle) * self.cam_to_tilt_dist 
-        cam_to_wall_y = math.tan(pan_angle) * self.cam_to_pan_dist 
+        cam_to_wall_x = self.cam_to_pan_dist * (math.tan(tilt_angle))
+        cam_to_wall_y = self.cam_to_tilt_dist * (math.tan(tilt_angle)) 
         print(cam_to_wall_x)
         print(cam_to_wall_y)
+
+        #cam_to_wall_x = self.cam_to_tilt_dist * (math.tan(tilt_angle))
+        #cam_to_wall_y = self.cam_to_pan_dist  * (math.tan(pan_angle)) 
+        return(cam_to_wall_x,cam_to_wall_y)
+
         #tjek om de giver den samme distance til væggen. returnerer om de giver den samme distance, og så en distance
         if cam_to_wall_x == cam_to_wall_y: 
                 self.cam_to_wall = cam_to_wall_x
