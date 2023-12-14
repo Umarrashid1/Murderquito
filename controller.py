90bec9b8e476f22fae58be86184d9ff1de2a7399
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2023-12-14 13:49:52 +0100

    rigtig controller!

--- controller.py
+++ controller.py
@@ -1,6 +1,8 @@
+import time
 import cv2
 import sys
 from camera import Camera
+from camera_calibration import CameraCalibrator
 from detection import Detector
 from servo_controller import  Servo_controller
 from servo_calibrator import  ServoCalibrator
@@ -12,34 +14,34 @@ if len(args)>0:
     input_param = int(args[0])
 else:
     input_param = None
+laser = Laser()
+laser.toggle_laser(0)
 
-servo_c = Servo_controller()
 cam = Camera(input_param)
 cv2.imwrite("ref_frame.jpg", cam.get_frame())
 det = Detector(cam)
+servo_c = Servo_controller()
 servo_cal = ServoCalibrator(cam, servo_c)
 laser = Laser()
 laser.toggle_laser()
+chosen_servo = 'y' #servox virker ikke. ved vinkelret bevæger den sig ikke ind i billedet.
+if chosen_servo == 'x': 
+    servo_cal.prepare_for_calibration(servo_c, chosen_servo)
+elif chosen_servo == 'y': 
+    servo_cal.prepare_for_calibration(servo_c, chosen_servo)
+servo_cal.find_indiv_center_angles(cam, det, servo_c, chosen_servo)        
+identical, distance = servo_cal.calc_dist_from_centerangle(servo_c)
 
-check = False
-if check is True:  #optional. for testing
-    frame = cam.run()#cam.get_frame()
-    chosen_servo = 'y' #servox virker ikke. ved vinkelret bevæger den sig ikke ind i billedet.
-    if chosen_servo == 'x': servo_cal.prepare_for_calibration(servo_c, chosen_servo)
-    elif chosen_servo == 'y': servo_cal.prepare_for_calibration(servo_c, chosen_servo)
-    while(check is False):
-        frame = cam.run()#cam.get_frame()
-        #check = servo_cal.center_laser(frame, det, servo_c) # virker!
-        check = servo_cal.find_indiv_center_angles(frame, det, servo_c, chosen_servo)        
-    identical, distance = servo_cal.calc_dist_from_centerangle(servo_c)
-
-check = False
 servo_cal.prepare_for_calibration(servo_c)
-while(check is False):
-    frame = cam.run()#cam.get_frame()
-    check = servo_cal.center_laser(frame, det, servo_c) # virker!      
+check = servo_cal.center_laser(cam, det, servo_c) # virker!      
 identical, distance2 = servo_cal.calc_dist_from_centerangle(servo_c)
 
+laser.toggle_laser()
+#time.sleep(120)
+ventehygge = input("wwwwaaaa")
+cam_cal = CameraCalibrator(cam.get_frame())
+cal_matrix, cal_dist = cam_cal.run_calibrations()
+'''
 while True:
     frame = cam.run()
     det.update_tracker(cam)
@@ -61,6 +63,7 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         laser.toggle_laser(0)
         break
+    '''
 # ##########    se evt:     ######################
 #   https://www.youtube.com/watch?v=1CVmjTcSpIw
 #   https://www.youtube.com/watch?v=sW4CVI51jDY&t=1202s
