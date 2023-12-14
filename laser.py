93d6548ab8fe01d17e4c7d12a8e252f86a8bcfb2
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2023-12-14 01:41:11 +0100

    laser class expanded with fire_pulse

--- laser.py
+++ laser.py
@@ -5,8 +5,11 @@ import RPi.GPIO as GPIO
 
 class Laser:
 
-    def __init__(self):
+    def __init__(self, pulse_ln = None):
         #self.io = pigpio.pi()
+        if pulse_ln is not None: 
+            self.pulse_length = pulse_ln
+        GPIO.setwarnings(False)
         self.pin = 16
         GPIO.setmode(GPIO.BCM) 
         GPIO.setup(self.pin, GPIO.OUT)
@@ -18,18 +21,18 @@ class Laser:
     def toggle_laser(self, command = None):
         if command is not None:
             if command == 1:
-                self._set_laser_on()
+                self.laser_status = self._set_laser_on()
                 return True
             elif command == 0: 
-                self._set_laser_off()
-                return True
+                self.laser_status = self._set_laser_off()
+                return False
             else:
                 print("toggle_laser() Error: Invalid argument.")
         elif self.laser_status == 0:
-            self._set_laser_on()
+            self.laser_status = self._set_laser_on()
             return True
         else:
-            self._set_laser_off()
+            self.laser_status = self._set_laser_off()
             return False
         
     def _set_laser_on(self):
@@ -40,6 +43,12 @@ class Laser:
     def _set_laser_off(self):
         self.laser_status = 0
         GPIO.output(self.pin, False)
-        return True
+        return False
     
-    def fire_laser_pulse(self): NotImplemented
+    def fire_laser_pulse(self):
+        self.laser_status = self._set_laser_on
+        time.sleep(self.pulse_length)
+        self.laser_status = self._set_laser_off
+    
+    def set_pulse_length(self, time_ln):
+        self.pulse_length = time_ln
