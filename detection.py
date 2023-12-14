80aef4009cc56cd6e58985226def267707a2bc70
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2023-12-14 15:22:11 +0100

    detection findred fix

--- detection.py
+++ detection.py
@@ -138,10 +138,28 @@ class Detector:
     def find_red (self, frame):
         #reference = cv2.imread("ref_frame.jpg")
 
-        lower = np.array([0, 4, 4])
-        upper = np.array([255, 15, 15])
+        img_hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
 
-        mask = cv2.inRange(frame, lower, upper)
+        # lower mask (0-10)
+        lower_red = np.array([0, 50, 50])
+        upper_red = np.array([10, 255, 255])
+        mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+        # upper mask (170-180)
+        lower_red = np.array([170, 50, 50])
+        upper_red = np.array([180, 255, 255])
+        mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+        # join my masks
+        mask = mask0 + mask1
+
+        # set my output img to zero everywhere except my mask
+        output_img = img.copy()
+        output_img[np.where(mask == 0)] = 0
+
+        # or your HSV image, which I *believe* is what you want
+        output_hsv = img_hsv.copy()
+        output_hsv[np.where(mask == 0)] = 0
         cv2.imwrite("testimg1213.jpg", mask)
         mask_contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for contour in mask_contours:
@@ -156,7 +174,7 @@ class Detector:
                 cx, cy = 0, 0
             print("center of mass is: ", cx, ",", cy)
 
-
+            return cx, cy
 
 
 
