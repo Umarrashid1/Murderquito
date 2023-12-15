cec7c3136e7f9017c379220f61782d0d8a9a3a87
umarrashid1 <umarrashid@hotmail.dk>
2023-12-15 19:53:53 +0100

    chans

--- servo.py
+++ servo.py
@@ -27,7 +27,7 @@ class Servo:
     def move(self, angle):
         self.angle = angle
         if self.axis == "x":
-            self.set_pwm(self.angle_pwm_conv(angle))
+            self.set_pwm(self.angle_pwm_conv(angle + 50))
         else:
             self.set_pwm(self.angle_pwm_conv(angle + 128))
 
