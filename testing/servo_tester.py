add739d9693bee564b291cae3ad596f583279cd5
umarrashid1 <umarrashid@hotmail.dk>
2023-12-17 14:10:31 +0100

    added manual servo

--- testing/servo_tester.py
+++ testing/servo_tester.py
@@ -12,8 +12,16 @@ import time
 from servo import Servo
 io = pigpio.pi()
 servo_x = Servo("x", io)
-for i in range(180):
-    servo_x.move(180)
-    print(i)
-    time.sleep(0.1)
-servo_x.move(0)
+servo_y = Servo("y", io)
+while True:
+    try:
+        x_val = int(input("Enter a value for servo_x to move: "))
+        y_val = int(input("Enter a value for servo_y to move: "))
+        
+        servo_x.move(x_val)
+        servo_y.move(y_val)
+    except ValueError:
+        print("Please enter a valid integer.")
+    except KeyboardInterrupt:
+        print("Exiting...")
+        break
