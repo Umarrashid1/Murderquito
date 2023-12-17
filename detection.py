e7e131934dc4ff288e5b644f9880e12f3fd10444
umarrashid1 <umarrashid@hotmail.dk>
2023-12-17 23:59:59 +0100

    changes

--- detection.py
+++ detection.py
@@ -7,7 +7,7 @@ class Detector:
     bbox = None
     tracker = None
     ok = False
-    TRACKER_TYPE = "TrackerBOOSTING"
+    TRACKER_TYPE = "TrackerBoosting"
     tracking_fail_counter = 0
     dupli_count = 0
     fail_array = []
